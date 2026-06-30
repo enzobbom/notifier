8c5a5733d80cd7961976c50f74824a5d8e04a3bd
Enzo Bom <enzobom@gmail.com>
2026-06-30 17:28:26 +0100

    Move retry config from Common to In RabbitMQ config class

--- src/main/java/com/javanauta/ts/notifier/adapters/config/RabbitCommonConfig.java
+++ src/main/java/com/javanauta/ts/notifier/adapters/config/RabbitCommonConfig.java
@@ -1,11 +1,5 @@
 package com.javanauta.ts.notifier.adapters.config;
 
-import com.javanauta.ts.notifier.adapters.out.email.exception.EmailException;
-import org.aopalliance.aop.Advice;
-import org.springframework.amqp.AmqpRejectAndDontRequeueException;
-import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
-import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -17,31 +11,5 @@ public class RabbitCommonConfig {
     @Bean
     public MessageConverter messageConverter() {
         return new JacksonJsonMessageConverter();
-    }
-
-    @Bean
-    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
-            ConnectionFactory connectionFactory,
-            MessageConverter messageConverter) {
-
-        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-
-        factory.setConnectionFactory(connectionFactory);
-        factory.setMessageConverter(messageConverter);
-
-        factory.setAdviceChain(retryAdvice());
-
-        return factory;
-    }
-
-    @Bean
-    public Advice retryAdvice() {
-
-        return RetryInterceptorBuilder.stateless()
-                .configureRetryPolicy(builder -> builder
-                        .maxRetries(3)
-                        .excludes(AmqpRejectAndDontRequeueException.class))
-                .backOffOptions(1000, 2.0, 5000)
-                .build();
     }
 }
--- src/main/java/com/javanauta/ts/notifier/adapters/in/messaging/config/RabbitInConfig.java
+++ src/main/java/com/javanauta/ts/notifier/adapters/in/messaging/config/RabbitInConfig.java
@@ -3,10 +3,16 @@ package com.javanauta.ts.notifier.adapters.in.messaging.config;
 import com.javanauta.ts.events.messaging.Exchanges;
 import com.javanauta.ts.events.messaging.Queues;
 import com.javanauta.ts.events.messaging.RoutingKeys;
+import org.aopalliance.aop.Advice;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -32,5 +38,30 @@ public class RabbitInConfig {
                 .bind(notificationRequestQueue)
                 .to(notificationExchange)
                 .with(RoutingKeys.NOTIFICATION_REQUEST);
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory,
+            MessageConverter messageConverter) {
+
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(messageConverter);
+
+        factory.setAdviceChain(retryAdvice());
+
+        return factory;
+    }
+
+    @Bean
+    public Advice retryAdvice() {
+        return RetryInterceptorBuilder.stateless()
+                .configureRetryPolicy(builder -> builder
+                        .maxRetries(3)
+                        .excludes(AmqpRejectAndDontRequeueException.class))
+                .backOffOptions(1000, 2.0, 5000)
+                .build();
     }
 }
